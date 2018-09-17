bd276485f03d4b384358efacb98ae3e0be319cd9
Shahjada Talukdar <shahjada.talukdar@gmail.com>
2018-09-17 18:02:17 +0200

    firebase config

--- app/app.js
+++ app/app.js
@@ -4,6 +4,16 @@ var app = angular.module("app", ['ngRoute', 'firebase']);
 
 app.config(function ($routeProvider, $locationProvider) {
 
+    var config = {
+        apiKey: "AIzaSyBobZeUv-PofD8shmd0Qx8y9OtnAs7Ifcw",
+        authDomain: "connect4-bc0dd.firebaseapp.com",
+        databaseURL: "https://connect4-bc0dd.firebaseio.com",
+        projectId: "connect4-bc0dd",
+        storageBucket: "connect4-bc0dd.appspot.com",
+        messagingSenderId: "798183539694"
+      };
+    firebase.initializeApp(config);
+
     $routeProvider
         .when("/", {
         templateUrl: "/views/c4.html",
